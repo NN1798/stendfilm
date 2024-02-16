0c1ada2f2d872ab4a3d8a3f861b5dfe0eb152552
NN <NN@example.com>
2024-02-17 03:39:09 +1000

    add models

--- people/models.py
+++ people/models.py
@@ -1,3 +1,170 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+def person_directory(instance, filename):
+    return 'media/{0}/{1}/{2}/{3}'.format(instance.first_name, instance.last_name, instance.birth_date, filename)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=105, verbose_name='Имя')
+    last_name = models.CharField(max_length=65, verbose_name='Фамилия')
+    photo = models.ImageField(upload_to=person_directory, default='-', blank=True, verbose_name='Фото')
+    birth_date = models.DateField()
+    age = models.PositiveIntegerField(validators=[
+                                                MaxValueValidator(130)
+                                            ]
+    )
+    fact = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Director(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='directors',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Монтажер'
+        verbose_name_plural = 'Монтажеры'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Screenwriter(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='screenwriters',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Сценарист'
+        verbose_name_plural = 'Сценаристы'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Producer(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='producers',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Продюсер'
+        verbose_name_plural = 'Продюсеры'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Operator(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='operators',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Оператор'
+        verbose_name_plural = 'Операторы'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Composer(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='composers',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Композитор'
+        verbose_name_plural = 'Композиторы'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Painter(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='painters',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Художник'
+        verbose_name_plural = 'Художники'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Editor(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='editors',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Монтажер'
+        verbose_name_plural = 'Монтажеры'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Actor(Person):
+    profession = models.ManyToManyField(
+        to='Profession',
+        related_name='actors',
+        verbose_name='Режиссер'
+    )
+
+    class Meta:
+        ordering = ('first_name', 'last_name')
+        verbose_name = 'Актер'
+        verbose_name_plural = 'Актеры'
+
+
+    def __str__(self):
+        return self.title
+
+
+class Profession(models.Model):
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Профессия'
+        verbose_name_plural = 'Профессии'
+
+
+    def __str__(self):
+        return self.title
