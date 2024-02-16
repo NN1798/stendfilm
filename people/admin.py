aa08835a5543b7a385207e312f009f4145712a3a
NN <NN@example.com>
2024-02-17 04:09:34 +1000

    register the models in admin

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Director, Screenwriter, Producer,
+    Operator, Composer, Painter,
+    Editor, Actor, Profession
+)
+
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Screenwriter)
+class ScreenwriterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Composer)
+class ComposerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Painter)
+class PainterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Editor)
+class EditorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name')
+    list_display_links = ('id', 'first_name', 'last_name')
+
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
