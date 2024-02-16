f5eea30e9c7b3c97f8d0b64fef9f2889108686e3
NN <NN@example.com>
2024-02-17 03:26:16 +1000

    update file settings.py

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compositions.apps.CompositionsConfig',
+    'people.apps.PeopleConfig',
 ]
 
 MIDDLEWARE = [
