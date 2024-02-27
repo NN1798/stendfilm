86b3ca02d42ba95297324b2ba2e8de5955cb9500
NN <NN@example.com>
2024-02-28 00:47:04 +1000

    register Film model in the admin panel

--- compositions/admin.py
+++ compositions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Film
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_year')
+    list_display_links = ('id', 'title')
+    list_filter = ('title', 'release_year')
+    search_fields = ('title',)
