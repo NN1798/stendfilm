ae70254c5e7d5ffe6b98bbbb935b5443d2a8348c
NN <NN@example.com>
2024-02-28 00:47:50 +1000

    register Serial model in the admin panel

--- compositions/admin.py
+++ compositions/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from .models import Film
+from .models import Film, Serial
 
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_year')
+    list_display_links = ('id', 'title')
+    list_filter = ('title', 'release_year')
+    search_fields = ('title',)
+
+
+@admin.register(Serial)
+class SerialAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'release_year')
     list_display_links = ('id', 'title')
     list_filter = ('title', 'release_year')
