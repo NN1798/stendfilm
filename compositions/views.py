ead176f7719331ada6a272686b41fbcd0bbce95b
NN1798 <andrey.makarevich.03@mail.ru>
2024-04-23 03:20:54 +1000

    add view for model Composition

--- compositions/views.py
+++ compositions/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Composition
+
+
+class CompositionListView(ListView):
+    model = Composition
+    template_name = 'compositions/index.html'
