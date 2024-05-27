197c4d4048ae67a49879887ecc98a577a0e94280
NN1798 <andrey.makarevich.03@mail.ru>
2024-05-28 01:41:33 +1000

    update admin.py

--- compositions/admin.py
+++ compositions/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
-from .models import Composition, Company, Genre, Country, Award
+from .models import Composition, Company, Genre, Country, Award, Rating, Comment
 
 
 @admin.register(Composition)
 class CompositionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'type', 'release_year')
+    list_display = ('id', 'title', 'type', 'release_year', 'get_average_rating', 'get_count_comment',)
     list_display_links = ('id', 'title')
     list_filter = ('title', 'release_year', 'type')
     search_fields = ('title',)
+
+    def get_average_rating(self, obj):
+        return obj.average_rating()
+    get_average_rating.short_description = 'Средний рейтинг'
+
+
+    def get_count_comment(self, obj):
+        return obj.count_comment()
+    get_count_comment.short_description = 'Кол-во комментариев'
 
 
 admin.site.register(Company)
@@ -21,3 +30,12 @@ admin.site.register(Country)
 
 
 admin.site.register(Award)
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('composition', 'score', 'user')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'comment', 'composition')
