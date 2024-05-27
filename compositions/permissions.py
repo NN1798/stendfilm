7ae5f43229a7bded2440794ab0aaf887032dcc46
NN1798 <andrey.makarevich.03@mail.ru>
2024-05-28 01:50:47 +1000

    add permissions for staff

---
+++ compositions/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+
+METHOD_UPDATE = ('PATCH', 'PUT')
+
+class IsStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_staff
+        return True
+    
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in METHOD_UPDATE:
+            return request.user.is_staff
+        return True
